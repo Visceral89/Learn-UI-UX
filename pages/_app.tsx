71965ab05f23187398cbd9e2fa2ab95f840b84c1
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-19 09:10:01 +0100

    WHY ISNT THIS WORKING
    Going insane.....slowly cant get this custom font to work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+
+const lemon = localFont({
+  src: [
+    {
+      path: '/fonts/LEMONMILK-Bold.woff2',
+      weight: '700' 
+    },
+    {
+      path: '/fonts/LEMONMILK-Regular.woff2',
+      weight: '400' 
+    },
+  ],
+  variable: '--font-lemon-bold',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<main className={`${lemon.variable} font-sans`}><Component {...pageProps} /></main>)
 }
