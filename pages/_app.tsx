f132919242f33b72e5c9bf4090da6955e2cd6a53
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-23 09:40:11 +0100

    Im so dumb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import localFont from '@next/font/local'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 
 const lemon = localFont({
-  src: [
-    {
-      path: '/fonts/LEMONMILK-Bold.woff2',
-      weight: '700' 
-    },
-    {
-      path: '/fonts/LEMONMILK-Regular.woff2',
-      weight: '400' 
-    },
-  ],
-  variable: '--font-lemon-bold',
-})
+	src: [
+		{
+			path: "/fonts/LEMONMILK-Bold.woff2",
+			weight: "700",
+		},
+		{
+			path: "/fonts/LEMONMILK-Regular.woff2",
+			weight: "400",
+		},
+	],
+	variable: "--font-lemon-bold",
+});
+
+console.log(lemon);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<main className={`${lemon.variable} font-sans`}><Component {...pageProps} /></main>)
+	return (
+		<main className={`${lemon.variable} font-sans`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
